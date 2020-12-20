32f04c4a2687daad260b96983806cab16505af99
Mohamed Boudjenane <mohaboudjenane@gmail.com>
2020-12-20 23:49:15 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+le_wagon_team = [
+  {'name': 'ben', 'age': 31, 'country': 'france', 'hobbies': ['coding', 'biking']},
+  {'name': 'quinn', 'age': 26, 'country': 'ireland', 'hobbies': ['skiing']},
+  {'name': 'sasha', 'age': 24, 'country': 'lebanon', 'hobbies': ['sports']},
+  {'name': 'alex', 'age': 28, 'country': 'austria', 'hobbies': []}
+]
+
+print(le_wagon_team)
+print(type(le_wagon_team))
+print(type('name'))
+print(len(le_wagon_team))
+print((le_wagon_team[0])
